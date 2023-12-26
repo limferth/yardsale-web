73070c869e3aeec17a7521ba598aa92ab43395d6
limferth <limferth@gmail.com>
2023-12-25 21:16:22 -0600

    se agrego el menu mobile con interactividad de js

--- script/script.js
+++ script/script.js
@@ -1,9 +1,17 @@
 const navEmail = document.querySelector('.nav-container-right__email');
-const desktopMenu = document.querySelector('.desktop-menu')
+const desktopMenu = document.querySelector('.desktop-menu');
+
+
+const burgerMenu = document.querySelector('.nav-container-imagen');
+const mobileMenu = document.querySelector('.mobile-menu')
 
 
 navEmail.addEventListener('click', toggleDesktopMenu);
+burgerMenu.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu (){
     desktopMenu.classList.toggle('inactive')
+}
+function toggleMobileMenu () {
+    mobileMenu.classList.toggle('inactive')
 }
